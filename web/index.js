89cb909dc7848595ac2deb40350300c48beb62f9
DayMan84 <129399117+DayMan84@users.noreply.github.com>
2025-12-09 20:32:22 -0600

    Update index.js

--- web/index.js
+++ web/index.js
@@ -1,16 +1,6 @@
-// ComfyUI-Usgromana-Gallery/web/js/index.js
+// ComfyUI-Usgromana-Gallery/web/index.js
 
 import { app } from "/scripts/app.js";
-import { initGalleryExtension, EXT_NAME } from "./gallery_ui.js";
-
-app.registerExtension({
-    name: EXT_NAME,
-    async setup() {
-        initGalleryExtension();
-    },
-});
-// web/index.js
-import { app } from "../../scripts/app.js";
 import { initGalleryExtension } from "./core/entry.js";
 
 app.registerExtension({
